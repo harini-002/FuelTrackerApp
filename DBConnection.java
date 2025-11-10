0e487bfce7b1d194b837191338c3aa7c1f89b69e
harini-002 <harinirm435@gmail.com>
2025-11-10 12:29:08 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,12 @@
+package dao;
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/fueltracker";
+    private static final String USER = "root"; // change if needed
+    private static final String PASSWORD = "Kabilesh@#$6126"; // your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
